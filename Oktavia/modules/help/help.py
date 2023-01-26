3618b3364fe21ab61d197e579305ffc4a0e8f3e2
amanqs <116822442+amanqs@users.noreply.github.com>
2023-01-26 18:25:12 +0700

    Update help.py

--- Oktavia/modules/help/help.py
+++ Oktavia/modules/help/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @xdbmusic_bot × @oktaviauserbot •",
+                f"```{str(ac)}```\n• @oktaviauserbot •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @xdbmusic_db × @oktaviauserbot •"
+            message, f"```{str(ac)}```\n• @oktaviauserbot •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -94,7 +94,7 @@ async def module_helper(client: Client, message: Message):
     if help_arg:
         if help_arg in CMD_HELP:
             commands: dict = CMD_HELP[help_arg]
-            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            this_command = f" **Help For {str(help_arg).upper()}** \n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
             this_command += "© @oktaviauserbot"

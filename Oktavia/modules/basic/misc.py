bcb47d692866e586d39e598f8deb9932764a5a81
amanqs <116822442+amanqs@users.noreply.github.com>
2023-01-26 16:40:50 +0700

    Update misc.py

--- Oktavia/modules/basic/misc.py
+++ Oktavia/modules/basic/misc.py
@@ -26,12 +26,12 @@ if ALIVE_TEXT:
    txt = ALIVE_TEXT
 else:
     txt = (
-f"** 𝗢𝗸𝘁𝗮𝘃𝗶𝗮𝗨𝘀𝗲𝗿𝗯𝗼𝘁 **\n",
-    f"**status**: [Admin]\n\n",
-          f" **oktavia_version**: `0.1`\n",
-          f" **peer_users: `13 users`",
-          f" **oktavia_uptime**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n",
-          f" **Order**: [ᴀᴍᴀɴɢ](t.me/amwang)\n",
+f"** 𝗢𝗸𝘁𝗮𝘃𝗶𝗮𝗨𝘀𝗲𝗿𝗯𝗼𝘁 **\n"
+    f"**status**: [Admin]\n\n"
+          f" **oktavia_version**: `0.1`\n"
+          f" **peer_users: `13 users`"
+          f" **oktavia_uptime**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
+          f" **Order**: [ᴀᴍᴀɴɢ](t.me/amwang)\n"
     )
 
 @Client.on_message(

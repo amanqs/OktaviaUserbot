16eb6b7674a1e0ee5ff83f5aba102d5fc0e07d1b
amanqs <116822442+amanqs@users.noreply.github.com>
2023-01-26 18:07:20 +0700

    Update misc.py

--- Oktavia/modules/basic/misc.py
+++ Oktavia/modules/basic/misc.py
@@ -28,11 +28,12 @@ else:
     txt = (
 f"** 𝗢𝗸𝘁𝗮𝘃𝗶𝗮𝗨𝘀𝗲𝗿𝗯𝗼𝘁 **\n"
            f"**status**: [Admin]\n"
-              f" **oktavia_uptime**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
+              f" **oktavia_uptime**: </b> <code>{uptime}</code>"
               f" **oktavia_version**: `0.1`\n"
               f" **expires_on**: `25.01.2028`\n"
               f" **peer_users**: `13 users`\n"
               f" **dc_id**: `5`\n"
+              f" **User : {message.from_user.mention}\n"
               f" **Order**: [ᴀᴍᴀɴɢ](t.me/amwang)\n"
     )
 
